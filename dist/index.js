6eee13b2b3a71b20ba3b4d37e76a9af48ac8c977
BIOZ <josefvacio@gmail.com>
2022-04-07 00:02:43 -0700

    Se agrego la funcionalidad para hacer el calculo pero ahora esta vez para el boton custom, el cual tu escribes el porcentaje y con la tecla enter se dispara la operacion

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
+const numberIntKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+const numberFloatKeys = [...numberIntKeys, "."]
+
+
 function getValue (idInput){
     const input = document.getElementById(idInput);
     const value = Number(input.value);
@@ -14,26 +18,30 @@ function getTotal (bill, people, individual){
     return total;
 }
 
-function makeOperation () {
-
+function eventNumber(keys, key, input) {
+    if (keys.includes(key) && key != "."){
+        const newValue = input.value + key
+        input.value = newValue;
+    } else if (key == "Backspace") {
+        const length = input.value.length;
+        input.value = input.value.slice(0, length - 1);
+    }
 }
 
 
 const inputPeople = document.getElementById("inputPeople");
 inputPeople.addEventListener("keydown", (e) => {
-    if (e.keyCode == "13"){
+    if(e.key == "Enter") {
         e.preventDefault();
     }
-    if (e.target.value  != 0) {
-        const message = document.getElementById("messageForPeople");
-        message.classList.replace("no-hidden", "hidden");
-    }
+    const message = document.getElementById("messageForPeople");
+    message.classList.replace("no-hidden", "hidden");
 });
 
 
 const inputBill = document.getElementById("inputBill");
 inputBill.addEventListener("keydown", (e) => {
-    if (e.keyCode == "13"){
+    if (e.key == "Enter") {
         e.preventDefault();
     }
 });
@@ -56,13 +64,28 @@ buttons.addEventListener("click", (e) => {
             const individual = getIndividual(bill, people, percent);
             const total = getTotal(bill, people, individual);
 
-            textIndividual.textContent = toFixed(individual, 2);
-            textTotal.textContent = toFixed(total, 2);
+            textIndividual.value = individual.toFixed(2);
+            textTotal.value = total.toFixed(2);
         }
     }
 })
 
+const buttonCustom = document.getElementById("buttonCustom");
 
-function changeMessage(className) {
-    
-}
+buttonCustom.addEventListener("keydown", (e) => {
+    if(e.key == "Enter") {
+        e.preventDefault();
+        const people = getValue("inputPeople");
+        const bill = getValue("inputBill");
+
+        const percent = buttonCustom.value;
+        const textIndividual = document.getElementById("individual");
+        const textTotal = document.getElementById("total");
+
+        const individual = getIndividual(bill, people, percent);
+        const total = getTotal(bill, people, individual);
+
+        textIndividual.value = individual.toFixed(2);
+        textTotal.value = total.toFixed(2);
+    }
+});
